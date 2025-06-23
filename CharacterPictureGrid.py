664e4443feb00287e93c47b6da6a59b8e0d69067
shruthi662 <shruthip8488@gmail.com>
2025-06-23 20:32:17 +0530

    Create CharacterPictureGrid.py

---
+++ CharacterPictureGrid.py
@@ -0,0 +1,17 @@
+grid = [
+    ['.', '.', '.', '.', '.', '.'],
+    ['.', 'O', 'O', '.', '.', '.'],
+    ['O', 'O', 'O', 'O', '.', '.'],
+    ['O', 'O', 'O', 'O', 'O', '.'],
+    ['.', 'O', 'O', 'O', 'O', 'O'],
+    ['O', 'O', 'O', 'O', 'O', '.'],
+    ['O', 'O', 'O', 'O', '.', '.'],
+    ['.', 'O', 'O', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.']
+]
+
+# Print the grid rotated 90 degrees clockwise
+for x in range(len(grid[0])):         # loop through columns
+    for y in range(len(grid)):        # loop through rows
+        print(grid[y][x], end='')
+    print()                           # new line after each row
